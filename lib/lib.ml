aaa2dd2333c4e6b42eafc634165d79233b58cbde
Andrew Smith <andrew.smith@wwinc.com>
2024-08-23 11:13:24 -0500

    Removed some comments.

--- lib/lib.ml
+++ lib/lib.ml
@@ -1,3 +1,2 @@
-(* lib/lib.ml *)
 module Subproject1 = Subproject1
 module Subproject2 = Subproject2
--- lib/subproject2.ml
+++ lib/subproject2.ml
@@ -1,4 +1,3 @@
-(* lib/subproject2.ml *)
 let run () =
   print_endline "Running Subproject 2";
   print_endline "Press Enter to return to the main menu";
