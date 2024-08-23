0f3b11dee22b4cffe25799f97ea8380731b98c27
Andrew Smith <andrew.smith@wwinc.com>
2024-08-23 11:31:19 -0500

    Implemented the times table.

--- lib/times_table.ml
+++ lib/times_table.ml
@@ -1,4 +1,13 @@
+let print_table size =
+  for i = 1 to size do
+    for j = 1 to size do
+      Printf.printf "%2d x %2d = %2d\n" i j (i * j)
+    done
+  done
+
 let run () =
-  print_endline "Running Subproject 1";
+  print_endline "Times Table";
+
+  print_table 12;
   print_endline "Press Enter to return to the main menu";
   ignore (read_line ())
