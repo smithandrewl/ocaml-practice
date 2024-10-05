dbbd1db26ab76a0caa996812b9c9a8aca61426a3
Andrew Smith <smith.andrew.l@gmail.com>
2024-10-05 17:59:10 -0500

    Added some integer array examples.

--- lib/array_practice.ml
+++ lib/array_practice.ml
@@ -4,6 +4,9 @@ let run () =
   print_endline "";
 
   let str_array = [|"One"; "Two"; "Three"; "Four"; "Five"; "Six" |] in
+  let int_array = [|1; 2; 3; 4; 5; 6; 7; 8; 9; 10|] in
+
+  printf "String array:\n"; 
 
   Array.iter 
     (fun word -> printf "%s\n" word)
@@ -15,3 +18,15 @@ let run () =
     (fun index value ->
       printf "item(%i) = %s\n" index value)
     str_array;
+
+  printf "\nInt array:\n\n";
+
+  printf "Even: ";
+  
+  int_array
+  |> Array.to_list
+  |> List.filter (fun value -> value mod 2 = 0)
+  |> Array.of_list
+  |> Array.iter (fun value -> printf "%i " value);
+  
+  printf "\n"
